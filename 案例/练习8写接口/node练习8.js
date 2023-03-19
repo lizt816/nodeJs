9367bfb0419230e3a6c99968cfbd594c264c0610
Mr.Li <1579436877@qq.com>
2023-03-19 22:57:27 +0800

    下一步学习mysql

--- 案例/练习8写接口/node练习8.js
+++ 案例/练习8写接口/node练习8.js
@@ -6,11 +6,24 @@
 
 const express = require('express')
 const router = require('./router/index')
+const cors = require('cors')
 
 const app = express()
+
+app.get("/api/jsonp",(req, res) => {
+    // 1，获取客户端发送过来的回调函数的名字
+    const funcName = req.query.callback
+    // 2.得到要通过 JSONP 形式发送给客户端的数据
+    const data = { name: 'zs' , age: 22 }
+    // 3.根据前两步得到的数据，拼接出一个函数调用的字符串
+    const scriptStr = `${funcName}(${JSON.stringify(data)})`// 4.把上一步拼接得到的字符串，响应给客户端的 <script> 标签进行解析执行
+    res.send(scriptStr)
+})
+
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use('/api',router)
 
 app.listen(80,()=>{
-    console.log('Express server running at http://127.0.0.1')
+    console.log('Express server running at http://127.0.0.1:80')
 })
--- 案例/练习8写接口/router/index.js
+++ 案例/练习8写接口/router/index.js
@@ -4,6 +4,7 @@ let router = express.Router()
 
 
 router.get('/getUserInfo',(req,res)=>{
+    console.log("-*-*-*-*")
     const query = req.query
     res.send({
        status:200,                    // 状态，表示请求成功
@@ -16,9 +17,19 @@ router.post('/postUserInfo',(req,res)=>{
     const body = req.body
     res.send({
         status:200,                    // 状态，表示请求成功
-        msg:'GET请求成功！',   // 描述状态
+        msg:'POST请求成功！',   // 描述状态
         data:body
      })
 })
+
+router.delete('/deleteUserInfo',(req,res)=>{
+    const body = req.body
+    res.send({
+        status:200,                    // 状态，表示请求成功
+        msg:'DELETE请求成功！',   // 描述状态
+        data:body
+     })
+})
+
 
 module.exports = router
