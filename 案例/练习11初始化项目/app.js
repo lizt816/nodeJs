fe17ee82e16200cce2aa9365dc78e664fc38f895
lizhitao <1579436877@qq.com>
2023-05-27 18:09:34 +0800

    初始化项目

--- 案例/练习11初始化项目/app.js
+++ 案例/练习11初始化项目/app.js
@@ -1,1 +1,16 @@
-var app = {}
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.urlencoded({extended:false}))
+
+
+
+
+app.use('/',express.static('./public'))
+const userRouter = require('./router/user.js')
+app.use('/api',userRouter)
+app.listen(1022,()=>{
+ console.log('api server running at  http://127.0.0.1:1022')
+})
+
