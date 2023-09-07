8e577a7243690224daf5cd98f7d408dfa2598f2d
lizt816 <1579436877@qq.com>
2023-09-07 17:20:49 +0800

    在练习10新增mysql数据，并且处理了list数据，用于存储JSON

--- 案例/练习10session/node练习10.js
+++ 案例/练习10session/node练习10.js
@@ -10,6 +10,54 @@ const express = require('express')
 const router = require('./router/index')
 const cors = require('cors')
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host:'127.0.0.1',                  // 数据库的 IP 地址
+    user:'root',                          // 登录数据库的账号
+    password:'admin123',      // 登录数据库的密码
+    database:'new_xiaohe'       // 指定要操作那个数据库
+  })
+
+
+// const sqlStr = 'select * from users'
+// db.query(sqlStr,(err,results)=>{
+//   if(err) return console.log(err.message)
+//   console.log(results[0],'查询结果')
+//   for (let i = 0; i < results.length; i++) {
+//    console.log(results[i].list)
+//   }
+// })
+
+// const user = { username:'new555',password:'1231',list:JSON.stringify({ "ddd": "111" })}
+// const sqlStr = 'INSERT INTO users SET ?'
+
+// db.query(sqlStr,user,(err,results)=>{
+//      if(err) return console.log(err.message)  // 失败
+//      if(results.affectedRows === 1){
+//         console.log('插入成功！！')
+//      }
+// })
+
+// const user = {id:4,username:'new4898**',password:'000'}
+// const sqlStr = 'UPDATE users SET ? where id=?'
+// db.query(sqlStr,[user,user.id],(err,results)=>{
+//    if(err) return console.log(err.message)
+//    if(results.affectedRows === 1){
+//        console.log('修改成功！！')
+//    }
+// })
+
+
+// const sqlStr = 'DELETE FROM users where id=?'
+// db.query(sqlStr,7,(err,results)=>{
+//    if(err) return console.log(err.message)
+//    if(results.affectedRows === 1){
+//        console.log('删除成功！！')
+//    }
+// })
+
+// 以上是关于Mysql的增删改查的操作
 
 // let session = require('express-session')
 // app.use(session({
